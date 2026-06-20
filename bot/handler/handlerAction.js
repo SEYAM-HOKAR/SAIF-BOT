c41fc50ff2e2e65e443472f7989e4cc225b1e2fc
SEYAM-HOKAR <seyamhokar@gmail.com>
2026-06-20 13:46:34 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,8 +72,8 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "🦋"){
-  if(event.userID == "100081317798618"){
+        if(event.reaction == "😹"){
+  if(event.userID == "61588880697306","61584002396785"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -82,8 +82,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction ==  "🦭"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100081317798618","100078639797619"){
+        if(event.reaction ==  "🙏"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61584002396785","61588880697306"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
